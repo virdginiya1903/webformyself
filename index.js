a5834f81ebe42875200c4574775dd5055cd9406f
virdginiya1903 <virdginiya1903@mail.ru>
2019-04-26 14:08:23 +0300

    Базовые операции

---
+++ index.js
@@ -0,0 +1,17 @@
+let num1 = 45;
+let num2 = 8;
+
+num1 = 99;
+console.log("+:", num1 + num2);
+console.log("-:", num1 - num2);
+console.log("*:", num1 * num2);
+console.log("/:", num1 / num2);
+
+let str1 = "Hello ";
+let str2 = "World";
+console.log("+:", str1 + str2);
+
+console.log("1 + 2:", 1 + "2");
+console.log("str1 + num1:", str1 + num1);
+console.log("True + 1:", true + 1);
+console.log("false + 1:", false + 1);
