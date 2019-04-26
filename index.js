f0eecaa8127c5a412023291d8bd66eacc0ef04a5
virdginiya1903 <virdginiya1903@mail.ru>
2019-04-26 14:17:09 +0300

    %,++,--,+= и т.п.

--- index.js
+++ index.js
@@ -15,3 +15,16 @@ console.log("1 + 2:", 1 + "2");
 console.log("str1 + num1:", str1 + num1);
 console.log("True + 1:", true + 1);
 console.log("false + 1:", false + 1);
+
+console.log("5 % 2:", 5 % 2);
+console.log("8 % 3:", 8 % 3);
+console.log("15 % 5:", 15 % 5);
+
+let i = 1;
+// i = 1 + 9;
+i += 9;
+i++; // увеличить на 1
+// i--; уменьшить на 1
+console.log("i", i);
+console.log("i", i++); // постфиксная запись, сначала выводится результат, а потом происходит сложенеи
+console.log("i", ++i); //префиксная: сначала сложение, потом результат
